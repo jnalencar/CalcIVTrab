39de337630973b6a15e371083b6728389d0d667f
Jonathan Neves <jonathannalencar@gmail.com>
2024-03-19 12:18:54 -0300

    ln now working, sqrt function added, new support menu

--- formulas.py
+++ formulas.py
@@ -3,13 +3,13 @@
 #Fonte: https://en.wikipedia.org/wiki/Leibniz_formula_for_π
 
 def fatorial(n):
-    if n == 0:
+    if n < 1 :
         return 1
     else:
         return n * fatorial(n-1)
 
 #e^x sendo x um número real calculado pela soma da série 1 + x + x^2/2! + x^3/3! + x^4/4! + ... + x^n/n!
-def exponcialx(x):
+def exponencialx(x):
     n = 50
     e = 1
     for i in range(1, n+1):
@@ -19,13 +19,28 @@ def exponcialx(x):
 #ln(x) esta quebrado
 # (?) Considerar numeros negativos??? (A série seria diferente)  -1 < x < 1
 #ln(x) sendo x um número real calculado pela soma da série x - x^2/2 + x^3/3 - x^4/4 + ... + (-1)^(n-1) * x^n/n
-#def ln(x):
-#    n = 100
-#    ln = 0
-#    for i in range(1, n+1):
-#        ln += (((-1)**(i+1)) * (x**i)/i)
-#    return ln
+def ln_using_mercator(y):
+    n = 100
+    
+    x = (y - 1) / (y + 1)
+    ln_y = 0
+    for i in range(n):
+        ln_y += (1 / (2*i + 1)) * (x ** (2*i + 1))
+    
+    return 2 * ln_y
 
+def sqrt_newton(x, tolerance=1e-10):
+    n = 100
+    y = x
+    for _ in range(n):
+        y_next = 0.5 * (y + x / y)
+        if abs(y - y_next) < tolerance:
+            break
+        y = y_next
+    
+    return y
+
+#π sendo calculado pela série de Leibniz
 def pi():
     n = 100000
     pi = 0
@@ -51,3 +66,20 @@ def cosx(x):
 
 def tanx(x):
     return senx(x)/cosx(x)
+
+def menu():
+    print("Escolha uma operação")
+    print("---Menu de Opções---")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("5 - Potenciação")
+    print("6 - Seno")
+    print("7 - Cosseno")
+    print("8 - Tangente")
+    print("9 - Exponencial (e^x)")
+    print("10 - Logaritmo Natural")
+    print("11 - Raiz Quadrada")
+    print("12 - Fatorial")
+    print("13 - Sair")
